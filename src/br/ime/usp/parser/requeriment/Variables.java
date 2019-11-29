ad374fd8c1d3365abfe587239f0b9333707b29d6
Alexandre Locci Martins <alexandre.locci@gmail.com>
2019-11-29 13:50:03 -0300

    update

--- src/br/ime/usp/parser/requeriment/Variables.java
+++ src/br/ime/usp/parser/requeriment/Variables.java
@@ -13,13 +13,13 @@ public class Variables {
         String exp = code.getCondition().toString();
         Expression e = new Expression(exp);
         mXparser mXparser = null;
-        //mXparser.consolePrintTokens(e.getCopyOfInitialTokens());
+        mXparser.consolePrintTokens(e.getCopyOfInitialTokens());
         List<Token> tokens = e.getCopyOfInitialTokens();
         //Object[] str = tokens.toArray();
        System.out.println(tokens.size() + "@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@");
-       for(int i=0; i < tokens.size(); i++) {
-           System.out.println(tokens.get(i).tokenStr  + "  " + tokens.get(i).tokenLevel + " " + tokens.get(i).looksLike + " " + tokens.get(i).keyWord);
-        }
+       //for(int i=0; i < tokens.size(); i++) {
+          // System.out.println(tokens.get(i).tokenStr  + "  " + tokens.get(i).tokenLevel + " " + tokens.get(i).looksLike + " " + tokens.get(i).keyWord);
+       // }
        return null;
 
     }
